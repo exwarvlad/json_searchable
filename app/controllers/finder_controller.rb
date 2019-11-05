b88568d5e6be656259e5cffe5f5c2b7edc3bcd79
exwarvlad <exwarvlad@gmail.com>
2019-11-05 16:18:55 +0200

    optimizated FinderService after code review

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -2,6 +2,6 @@ class FinderController < ApplicationController
   def index; end
 
   def find_out
-    render 'finder/finder', locals: { finder: FinderService.new(params).call }
+    render 'finder/finder', locals: { finder: FinderService.new(params, FOUNDER).call }
   end
 end
--- app/services/finder_service.rb
+++ app/services/finder_service.rb
@@ -4,25 +4,22 @@ require 'open-uri'
 class FinderService
   attr_reader :params
 
-  def initialize(params)
-    @params = JSON(params.permit(:name, :type, :designed).to_json, symbolize_names: true).delete_if { |_k, v| v.blank? }
-    @founder = FOUNDER
+  def initialize(params, founder = FOUNDER)
+    @params = require_params(params)
+    @founder = founder
   end
 
   def call
-    result = params.each_key {|k| @founder = send("find_by_#{k}")}
-
-    if result.blank? || @founder.blank?
-      nil
-    else
-      @founder
-    end
+    result = params.keys.each { |k| self.founder = send("find_by_#{k}") }
+    result.blank? ? nil : founder
   end
 
   private
 
+  attr_accessor :founder
+
   def find_by_name
-    result = @founder&.bsearch { |status| params[:name] <=> status['Name'] }
+    result = founder&.bsearch { |status| params[:name] <=> status['Name'] }
     [result] if result
   end
 
@@ -35,15 +32,22 @@ class FinderService
   end
 
   def search_match(founder_key, param)
-    @founder&.select do |hash|
-      attr = hash[founder_key].delete(' ').split(',')
-      has_match?(attr, param.delete(' '))
-    end
+    result =
+      founder&.select do |hash|
+        attr = hash[founder_key].delete(' ').split(',')
+        has_match?(attr, param.delete(' '))
+      end
+
+    self.founder = result if result.present?
   end
 
   def has_match?(array, param)
     has_match = false
-    array.each { |item| break has_match = true if item =~ /#{param}/  }
+    array.each { |item| break has_match = true if item =~ /#{param}/ }
     has_match
+  end
+
+  def require_params(params)
+    JSON(params.permit(:name, :type, :designed).to_json, symbolize_names: true).select { |_k, v| v.present? }
   end
 end
